22314465227c653f3cddf33b814dd7f81e565e8a
Ralf S. Engelschall <rse@engelschall.com>
2020-08-12 21:52:53 +0200

    use decent colors related to my usual theme

--- hud-widget-confetti.vue
+++ hud-widget-confetti.vue
@@ -71,6 +71,10 @@ module.exports = {
                         ticks:         1000,
                         origin:        { x: 0.5, y: 1.0 },
                         shapes:        [ "circle", "square" ],
+                        colors:        [ "#909090", "#c0c0c0", "#ffffff"  /* white */,
+                                         "#336699", "#6699cc", "#99ccff", /* blue  */
+                                         "#b06820", "#e09850", "#ffb870", /* brown */
+                                         "#cc3333"                        /* red */  ],
                         zIndex:        1000
                     }, opts, {
                         angle:         randomInRange(55, 125),
